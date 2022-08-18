174437ecdabef567329338ce7584d51f9b5f6c5f
Soheel Haque-Everding <soheelhaque@hotmail.com>
2022-08-18 17:25:46 +0200

    Updated setup.sh to install CryptoAlgo first then QuestDb

--- Scripts/setup.sh
+++ Scripts/setup.sh
@@ -4,13 +4,11 @@ exec > >(tee /var/log/user-data.log|logger -t user-data -s 2>/dev/console) 2>&1
 # Update the box to the latest software
 sudo yum update -y
 
+# Set the timezone correctly for cron
+sudo timedatectl set-timezone Europe/London
+
 # Install git
 sudo yum install git -y
-
-# Install QuestDB
-cd ~
-wget https://github.com/questdb/questdb/releases/download/6.4.3/questdb-6.4.3-rt-linux-amd64.tar.gz
-tar xvf questdb-6.4.3-rt-linux-amd64.tar.gz
 
 # Install CryptoAlgo
 cd ~
@@ -18,8 +16,19 @@ git clone https://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@github.com/soheelhaqu
 
 # Install requirements for CryptoAlgo
 sudo yum install -y gcc postgresql-devel
-sudo yum install python3-devel
+sudo yum install -y python3-devel
 sudo python3 -m pip install -r ~/CryptoAlgo/requirements.txt
+
+# Make all the scripts executable
+sudo chmod +x ~/CryptoAlgo/Scripts/*
+
+# Install QuestDB
+cd ~
+wget https://github.com/questdb/questdb/releases/download/6.4.3/questdb-6.4.3-rt-linux-amd64.tar.gz
+tar xvf questdb-6.4.3-rt-linux-amd64.tar.gz
+
+# Create QuestDB drive from snapshot
+sudo ~/CryptoAlgo/Scripts/restore_questdb_snapshot.sh
 
 # Setup systemd to run QuestDB
 sudo cp ~/CryptoAlgo/Scripts/questdb.service /etc/systemd/system/questdb.service
