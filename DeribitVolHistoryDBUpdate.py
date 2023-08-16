0c5d3843a6e9053f42d1c0fdbe6a7dbe3f0e720f
fred <fred.tingey@gmail.com>
2023-08-16 14:00:54 +0100

    extended vol history to 2017

--- DeribitVolHistoryDBUpdate.py
+++ DeribitVolHistoryDBUpdate.py
@@ -598,7 +598,7 @@ class DeribitVolHistoryDBUpdate:
             inserting any data missing from the Vol History table
         """
 
-        years = [2019, 2020, 2021, 2022, 2023]
+        years = [2017, 2018, 2019, 2020, 2021, 2022, 2023]
         months = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
 
         for year in years:
